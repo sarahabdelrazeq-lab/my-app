9c1a4b3466a189b58af2f6e24441c53ec8ee7169
sarah.abdelrazeq <sarah.abdelrazeq@opensooq.com>
2025-12-08 17:11:17 +0300

    rrrrrrrrrrrrrrrrrrrrr

--- app/page.tsx
+++ app/page.tsx
@@ -45,19 +45,6 @@ export default function Home() {
           <input />
         </div>
       </div>
-
-      <div style={{ height: 0 }}>
-        <audio
-          controls
-          style={{ opacity: 0, width: 0, height: 0 }}
-          preload="auto"
-        >
-          <source
-            src="https://arbok.sooqdev3.com/originals/chat/d0/0b/d00bc8994b054e6a3d25fbac91c35537e46ae94104f30af1af4315fac61c29d0.mp3"
-            type="audio/mpeg"
-          />
-        </audio>
-      </div>
     </main>
   );
 }
