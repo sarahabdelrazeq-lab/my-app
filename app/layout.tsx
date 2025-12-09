103ae521d78d6bd4220432fd481673e8c51b2439
sarah.abdelrazeq <sarah.abdelrazeq@opensooq.com>
2025-12-09 10:49:43 +0300

    dddddddddddddddddddddddddddd

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, interactive-widget=resizes-content"></meta>
+      </head>
       <body>{children}</body>
     </html>
   );
